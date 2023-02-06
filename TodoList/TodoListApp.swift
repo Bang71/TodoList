e0b7d6e459121ee326732f0fb6d212ed44ed5611
bang-71 <es.bang71@icloud.com>
2023-02-06 14:21:11 +0900

    init project

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by 신병기 on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
